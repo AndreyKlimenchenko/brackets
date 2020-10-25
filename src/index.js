d279d2fbefe5e63a79a8e040d99a04c1144378c8
Андрей Клименченко <andreyklim@iMac-Andrej.local>
2020-10-25 18:09:05 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+
+var bracketsLeft = ['{', '(', '['];
+var bracketRight = ['}', ')', ']'];
+
+module.exports = function check(str, bracketConfig) {
+  var openingChars = [];
+  var closingChars = [];
+var theSame =false;
+
+  bracketConfig.flat().forEach( (item) => {
+    if(item === '|'){
+     theSame = true;
+    }
+    if(bracketsLeft.indexOf(item) >-1){
+      openingChars.push(item);
+    } else if (bracketRight.indexOf(item) > -1){
+      closingChars.push(item);
+    }
+  });
+
+  var stack = [];
+var theSameBrackets = [];
+  for (var i = 0, len = str.length; i < len; i++) {
+      var char = str[i];
+      var openIdx = openingChars.indexOf(char);
+      var closeIdx = closingChars.indexOf(char);
+    if(theSame && char === '|'){
+      theSameBrackets.push(i)
+    }
+      if (openIdx > -1) {
+          stack.push(openIdx);
+      } else if (closeIdx > -1) {            
+          if (stack.length === 0) return false;
+          lastIdx = stack.pop();
+          if(lastIdx !== closeIdx) return false;
+      }
+  }
+
+if(theSameBrackets.length !==0 && theSameBrackets.length % 2 === 0) {
+      return true;
 }
+  if (stack.length !== 0) return false;
+  return true;     
+};
